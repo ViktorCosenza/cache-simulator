82e0541dd8cc215575f2e873a4a7111cf31e8a11
Victor Cosenza <vcvdsilva@inf.ufpel.edu.br>
2018-11-04 19:13:10 -0200

    correcao no import

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import os
 import sys
-from myCache import CacheSim as Cache
+from myCache import CacheSim
 
 def main():
     bsize, nsets, assoc =  sys.argv[0], sys.argv[1], sys.argv[2]
-    c = Cache(bsize, nsets, assoc)
+    c = CacheSim(bsize, nsets, assoc)
     
 
 
