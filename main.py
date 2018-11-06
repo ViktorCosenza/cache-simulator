bdb8330ddbf89b98cceaa1faccd2dac8b69e4620
Victor Cosenza <vcvdsilva@inf.ufpel.edu.br>
2018-11-06 18:06:28 -0200

    att TODOs

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 import sys
 from myCache import CacheSim
 
+#TODO adequar para parametro de entrada tipo "./main.py <bsize>:<nsets>:<assoc>"
+#TODO gerador de endereços ou maneira de portar o que o professor forneceu em java
 def main():
     bsize, nsets, assoc =  sys.argv[0], sys.argv[1], sys.argv[2]
     c = CacheSim(bsize, nsets, assoc)
